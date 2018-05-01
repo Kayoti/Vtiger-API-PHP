d3eb2ff868436915c811a1717d8cb7a0f35d063f
Wael <ymnwael@gmail.com>
2018-05-01 17:08:53 -0600

    removed some functions from auth class

--- controller/VtApiClasses/Webservices/crm_auth.php
+++ controller/VtApiClasses/Webservices/crm_auth.php
@@ -19,51 +19,6 @@ class crm_auth    // Class Start
      $this->sessionid=$this->login['result']['sessionName'];
 
   }
-  /**
-   * Gets the crm challenge token.
-   *
-   * @return $challengeToken
-   */
-  public function getchallengeToken()
-  {
-      return $this->challengeToken;
-  }
-
-  /**
-   * Sets the crm challenge token.
-   *
-   * @param  $challengeToken
-   *
-   * @return self
-   */
-  public function setchallengeToken($challengeToken)
-  {
-      $this->challengeToken = $challengeToken;
-
-
-  }
-  /**
-   * Gets the crm sessionid.
-   *
-   * @return $sessionid
-   */
-  public function getsessionid()
-  {
-      return $this->sessionid;
-  }
-
-  /**
-   * Sets the crm sessionid.
-   *
-   * @param  $sessionid
-   * @param  $crmobj
-   *
-   * @return self
-   */
-  public function setsessionid($sessionid)
-  {
-    $this->sessionid = $sessionid;
-  }
 }
 
 
