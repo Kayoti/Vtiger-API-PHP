207606e003e441a047e023299351d94eb0d4deee
kayoti <ymnwael@gmail.com>
2023-12-14 01:07:36 -0700

    quick variable name fix

--- Model/Const.php
+++ Model/Const.php
@@ -5,8 +5,8 @@ require_once __DIR__. '/../vendor/autoload.php';
 session_start();
 //CRM Credentials ...for security purposes please use php environment variables read more about getenv and php environment variables to prevent security issues
 
-$endpointUrl = getenv('CRMURL');    // [YOUR CRM URL ENVIRONMENT VARIABLE HERE] YOUR URL MUST END WITH /webservice.php eg(http://example.com/vtigercrm/webservice.php) 
-$userName = getenv('CRMUSERNAME');    // [YOUR CRM USER ENVIRONMENT VARIABLE HERE] // recommend  CRM USER with admin previliage
-$userAccessKey = getenv('CRMAPIKEY'); // [YOUR CRM USER ACCESS KEY ENVIRONMENT VARIABLE HERE] //access key of the user admin, found on my preferences page.
+$EndpointUrl = getenv('CRMURL');    // [YOUR CRM URL ENVIRONMENT VARIABLE HERE] YOUR URL MUST END WITH /webservice.php eg(http://example.com/vtigercrm/webservice.php) 
+$UserName = getenv('CRMUSERNAME');    // [YOUR CRM USER ENVIRONMENT VARIABLE HERE] // recommend  CRM USER with admin previliage
+$UserAccessKey = getenv('CRMAPIKEY'); // [YOUR CRM USER ACCESS KEY ENVIRONMENT VARIABLE HERE] //access key of the user admin, found on my preferences page.
 
  ?>
